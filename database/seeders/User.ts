8297c67fea7e0193ee31f9b199c6c8a02e5ae7fb
Wancieho <wancieho@gmail.com>
2023-10-03 18:12:20 +0200

    add seeder primary keys

--- database/seeders/User.ts
+++ database/seeders/User.ts
@@ -5,10 +5,12 @@ export default class extends BaseSeeder {
   public async run() {
     await User.createMany([
       {
+        id: 17,
         name: 'Piet',
         email: 'piet@gmail.com',
       },
       {
+        id: 23,
         name: 'Gert',
         email: 'gert@yahoo.co.uk',
       },
